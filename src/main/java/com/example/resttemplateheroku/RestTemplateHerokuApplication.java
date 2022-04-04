5fb49a621e78661d5a3689e638cc1d53910956e9
fveloci <fveloci@hotmail.com>
2022-04-04 17:30:38 -0300

    Add eureka

--- src/main/java/com/example/resttemplateheroku/RestTemplateHerokuApplication.java
+++ src/main/java/com/example/resttemplateheroku/RestTemplateHerokuApplication.java
@@ -2,8 +2,10 @@ package com.example.resttemplateheroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RestTemplateHerokuApplication {
 
 	public static void main(String[] args) {
